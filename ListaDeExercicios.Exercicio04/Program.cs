bfa9333b2d107d05e77c3851a5c5b0f514a8b9fa
Calebe <calebepfinger@gmail.com>
2025-04-23 21:34:48 -0300

    Implementação do imput e output

---
+++ ListaDeExercicios.Exercicio04/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ListaDeExercicios.Exercicio04
+{
+    internal class Program
+    {
+        //Crie um programa para converter a temperatura da escala Celsius para a escala Fahrenheit
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("Converter graus Celsius para Fahrenheit");
+            Console.WriteLine("--------------------------------------------------");
+
+            #region imput de dados
+            Console.WriteLine("Qual a temperatura em graus Celsius: ");
+            double celsius = Convert.ToDouble(Console.ReadLine());
+            #endregion
+
+            #region processamento de dados
+            double fahrenheit = (celsius * (9.0 / 5)) + 32;
+            #endregion
+
+            #region output de dados
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine($"A temperatura de {celsius}°C equivale a {fahrenheit}°F");
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
